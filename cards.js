770c1a960cf3fcdca57c6b22156ad328697d02a4
AdrienW86 <adrien_weiss@outlook.fr>
2025-08-26 16:14:05 +0200

    mise en ligne

--- cards.js
+++ cards.js
@@ -6,4 +6,13 @@ export const cards = [
   { category: "Cartes Catastrophe", name: "Chakra explosé", description: "Trop de yoga, ton aura implose.", image: "/chakra.png" },
   { category: "Cartes Catastrophe", name: "Pendu du tarot", description: "Tu restes coincé à l’envers, game over.", image: "/pendu.png" },
 
+    // CARTES DEFENSES
+  { category: "Cartes Défenses", name: "Améthyste anti-drama", description: "Bloque une Catastrophe.", image: "/amethyste.png" },
+  { category: "Cartes Défenses", name: "Sauge sacrée turbo", description: "Purifie l’espace, Catastrophe annulée.", image: "/sauge.png" },
+  { category: "Cartes Défenses", name: "Alignement cosmique", description: "Redirige la Catastrophe vers un autre joueur.", image: "/alignement.png" },
+  { category: "Cartes Défenses", name: "Bâton de sorcière d'occasion", description: "Fonctionne, même si c’est cheap. Protège une fois.", image: "/baton.png" },
+  { category: "Cartes Défenses", name: "Bouclier d’obsidienne", description: "Absorbe la tuile.", image: "/bouclier.png" },
+  { category: "Cartes Défenses", name: "Oracle grincheux", description: "Râle si fort qu’il fait fuir la tuile.", image: "/grincheux.png" },
+  { category: "Cartes Défenses", name: "Talisman en toc", description: "Ça ne devrait pas marcher, mais ça marche quand même.", image: "/talisman.png" },
+
  ]
