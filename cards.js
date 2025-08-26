739b9d5d3f1519b92317d4f709a302d7314fb660
AdrienW86 <adrien_weiss@outlook.fr>
2025-08-26 13:49:30 +0200

    mise en ligne

---
+++ cards.js
@@ -0,0 +1,9 @@
+export const cards = [
+  // CARTES CATASTROPHES
+  { category: "Cartes Catastrophe", name: "Boule de cristal", description: "Ton futur est trop moche, tu quittes la partie.", image: "/boule-cristal.png" },
+  { category: "Cartes Catastrophe", name: "Encens discount", description: "Les esprits se vexent et te punissent.", image: "/invocation.png" },
+  { category: "Cartes Catastrophe", name: "Invocatioçn ratée", description: "Au lieu d’un démon, tu invoques une chèvre.", image: "/chevre.png" },
+  { category: "Cartes Catastrophe", name: "Chakra explosé", description: "Trop de yoga, ton aura implose.", image: "/chakra.png" },
+  { category: "Cartes Catastrophe", name: "Pendu du tarot", description: "Tu restes coincé à l’envers, game over.", image: "/pendu.png" },
+
+ ]
